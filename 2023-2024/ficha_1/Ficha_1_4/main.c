4d0758ab9f8d6dd10a072ddb16bd0e40f77d60ee
Joao Bett <joao.gouveia.bettencourt@gmail.com>
2023-09-19 12:38:24 +0100

    feat: inicio ficha1 ex 4

---
+++ 2023-2024/ficha_1/Ficha_1_4/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1, num2;
+    printf("Introduza o primeiro numero: ");
+    scanf("%d", &num1);
+    printf("\nIntroduza o segundo numero: ")
+    scanf("%d", &num2);
+    printf("\n\nsoma\t\t %d + %d = ", num1,num2, soma);
+    printf("\nsubtracao");
+    printf("\nmultiplicacao");
+    printf("\ndivisao");
+    printf("\nresto da divisao inteira");
+    return 0;
+}
