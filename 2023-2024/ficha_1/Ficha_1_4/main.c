aa275b8659603289040d37a77a21bfa2430dd960
Joao Bett <joao.gouveia.bettencourt@gmail.com>
2023-09-19 22:47:33 +0100

    refactor: ficha1 ex4

--- 2023-2024/ficha_1/Ficha_1_4/main.c
+++ 2023-2024/ficha_1/Ficha_1_4/main.c
@@ -3,15 +3,20 @@
 
 int main()
 {
-    int num1, num2;
+    int num1, num2, soma, sub, multi, div, resto;
     printf("Introduza o primeiro numero: ");
     scanf("%d", &num1);
-    printf("\nIntroduza o segundo numero: ")
+    printf("\nIntroduza o segundo numero: ");
     scanf("%d", &num2);
-    printf("\n\nsoma\t\t %d + %d = ", num1,num2, soma);
-    printf("\nsubtracao");
-    printf("\nmultiplicacao");
-    printf("\ndivisao");
-    printf("\nresto da divisao inteira");
+    soma = num1 + num2;
+    sub = num1 - num2;
+    multi = num1 * num2;
+    div = num1 / num2;
+    resto = num1 % num2;
+    printf("\n\nsoma\t\t %d + %d = %d", num1,num2, soma);
+    printf("\nsubtracao\t %d - %d = %d", num1,num2, sub);
+    printf("\nmultiplicacao    %d * %d = %d", num1,num2, multi);
+    printf("\ndivisao\t\t %d / %d = %d", num1,num2, div);
+    printf("\nresto da divisao inteira %d %% %d = %d", num1,num2, resto);
     return 0;
 }
