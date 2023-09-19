b1cde23a990f69d96dbf5cdd00601b4a0c5aed09
Joao Bett <joao.gouveia.bettencourt@gmail.com>
2023-09-19 12:00:16 +0100

    feat: ficha1

---
+++ 2023-2024/ficha_1/Ficha_1/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num;
+    printf("Hello world!\n");
+    printf("\tExemplo de tabulacao\n\n");
+
+    printf("Indique um numero:\n");
+    scanf("%d", &num);
+    printf("O numero que indicou foi o %d", num);
+    return 0;
+}
