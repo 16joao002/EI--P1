193511c249cb54f77d2281d5e02a3e38902e835c
Joao Bett <joao.gouveia.bettencourt@gmail.com>
2023-09-25 12:28:50 +0100

    feat: ficha2 ex2

--- 2023-2024/ficha_2/Ficha_2_2/main.c
+++ 2023-2024/ficha_2/Ficha_2_2/main.c
@@ -3,6 +3,19 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int num1,num2,num3;
+    printf("Insira o primeiro numero\n");
+    scanf("%d", &num1);
+    printf("Insira o segundo numero\n");
+    scanf("%d", &num2);
+    printf("Insira o terceiro numero\n");
+    scanf("%d", &num3);
+
+    if(num1 != num2 || num1 != num3 || num2 != num3){
+        printf("Os numeros sao diferentes entre si");
+    }
+    else{
+        printf("Os numeros sao iguais entre si");
+    }
     return 0;
 }
